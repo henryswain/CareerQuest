a23bec5d372577ca2f16d5a330b3212fc66691e7
tle18 <tle18@uiowa.edu>
2025-04-13 15:38:57 -0500

    Error in findjobs fixed

--- src/pages/FindJobs.vue
+++ src/pages/FindJobs.vue
@@ -246,18 +246,6 @@
           </ul>
         </div>
       </div>
-      <div class="footer-section">
-        <img class="footer-logo" src="@/assets/CQ_logo_lightmode.svg" alt="CareerQuest Logo" />
-      </div>
-      <div class="footer-section">
-        <ul class="footer-links">
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li><a href="/privacy">Privacy</a></li>
-          <li><a href="/terms">Terms</a></li>
-        </ul>
-      </div>
-    </div>
   </footer>
 </template>
 
