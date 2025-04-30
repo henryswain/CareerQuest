cec2f3dd5cbefc5e9dc13d739b7e1cc0275e4bc6
cjwallerich <cjwallerich@uiowa.edu>
2025-04-30 12:01:21 -0500

    fixed giant "block of white space" on findjobs

--- src/router.js
+++ src/router.js
@@ -3,8 +3,6 @@ import HomePage from './pages/HomePage.vue';
 import FindJobs from './pages/FindJobs.vue'; 
 import ProfilePage from './pages/ProfilePage.vue';
 import SavedJobs from './pages/SavedJobs.vue';
-import LoginPage from './pages/Login.vue';
-import RegisterPage from './pages/Register.vue';
 import Settings from './pages/Settings.vue';
 import AboutPage from './pages/About.vue';
 import ContactPage from './pages/Contact.vue';
@@ -39,16 +37,6 @@ const routes = [
     path: '/saved-jobs',
     name: 'SavedJobs',
     component: SavedJobs,
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: LoginPage,
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: RegisterPage,
   },
   {
     path: '/settings',
